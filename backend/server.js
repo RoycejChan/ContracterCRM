19a443370289d6470827f535f6c761d899ff97d6
Roycej-Chan <Rouyce23@gmail.com>
2023-11-27 19:26:53 -0800

    alble to add data into sql db

--- backend/server.js
+++ backend/server.js
@@ -16,27 +16,72 @@ app.use(
   );
   
 
-
-app.get('/Contacts', (req, res) => {
+  app.post('/Contacts', (req, res) => {
     const { newContact } = req.body;
-    console.log(newContact);
-
-    const query = 'SELECT * FROM Contacts';
-    // const values = [newContact.first_name, newContact.last_name, newContact.email];
-
-
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error('Error executing query:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json(results);
-    }
+  
+    // Extract properties from newContact
+    const {
+      firstName,
+      lastName,
+      accountName,
+      email,
+      workPhone,
+      mobilePhone,
+      assistantName,
+      title,
+      department,
+      fax,
+      dob,
+      asstPhone,
+    } = newContact;
+  
+    // Construct SQL query with placeholders for optional fields
+    const query =
+      'INSERT INTO contact (firstName, lastName, accountName, email, workPhone, mobilePhone, assistantName, title, department, fax, dob, asstPhone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  
+    // Placeholders avoid SQL injection
+    const values = [
+      firstName,
+      lastName,
+      accountName,
+      email,
+      workPhone,
+      mobilePhone,
+      assistantName,
+      title,
+      department,
+      fax,
+      dob,
+      asstPhone,
+    ];
+  
+    connection.query(query, values, (error, results) => {
+      if (error) {
+        console.error('Error executing query:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+        console.log('Contact added successfully');
+        res.json(results);
+      }
+    });
   });
-});
+  
+  
+  
 
 
+// app.get('/Contacts', (req, res) => {
+//   const query = 'SELECT * FROM Contacts';
 
+//   connection.query(query, (error, results) => {
+//     if (error) {
+//       console.error('Error executing query:', error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     } else {
+//       res.json(results);
+//     }
+//   });
+// });
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- frontend/src/components/createNew/newContact/createContact.tsx
+++ frontend/src/components/createNew/newContact/createContact.tsx
@@ -1,63 +1,118 @@
-
-const backendURL = 'http://localhost:3000';
-
-import CreateNewNav from "../createNav/createNav"
-import "./newContact.css"
-
-import { Input } from '@chakra-ui/react'
-import { Stack } from "@chakra-ui/react"
-
-import pfp from "../../../assets/otherpfp.png"
+import { useState } from 'react';
+import CreateNewNav from "../createNav/createNav";
+import "./newContact.css";
+import { Input } from '@chakra-ui/react';
+import { Stack } from "@chakra-ui/react";
+import pfp from "../../../assets/otherpfp.png";
 
 export default function CreateContact() {
+  const [contactInfo, setContactInfo] = useState({
+    firstName: '',
+    lastName: '',
+    accountName: '',
+    email: '',
+    workPhone: '',
+    mobilePhone: '',
+    assistantName: '',
+    title: '',
+    department: '',
+    fax: '',
+    dob: '',
+    asstPhone: ''
+  });
 
-    async function check() { 
-        try {
-            const response = await fetch(`${backendURL}/Contacts`);
-            if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-          } catch (error) {
-            console.error('Error fetching user data:', error);
-          }
+  const backendURL = 'http://localhost:3000';
+
+  async function check() {
+    try {
+      const response = await fetch(`${backendURL}/Contacts`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ newContact: contactInfo }),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      console.log("hello");
+      const data = await response.json();
+      console.log('Server response:', data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
     }
+  }
+  
 
-    return (
-        <>
+  const inputFields = [
+    { label: 'First Name', key: 'firstName', required: true },
+    { label: 'Last Name', key: 'lastName', required: true },
+    { label: 'Account Name', key: 'accountName', required: true },
+    { label: 'Email', key: 'email' },
+    { label: 'Work Phone', key: 'workPhone' },
+    { label: 'Mobile Phone', key: 'mobilePhone', required: true },
+    { label: 'Assistant Name', key: 'assistantName' },
+    { label: 'Title', key: 'title', required: true },
+    { label: 'Department', key: 'department', required: true },
+    { label: 'Fax', key: 'fax' },
+    { label: 'Date of Birth', key: 'dob', type: 'date' },
+    { label: 'Asst Phone', key: 'asstPhone' }
+  ];
 
-           <CreateNewNav page="Contact"/> 
+  const handleInputChange = (field:any, value:any) => {
+    setContactInfo(prevInfo => ({ ...prevInfo, [field]: value }));
+  };
 
-        <div className="new-container">
-            <div className="new-PFP">
-                <h1 className="text-2xl">Contact Image</h1>
-                <div className="contactpfp">
-                    <img src={pfp} alt="PFP" />
-                </div>
-            </div>
-            <div className="newContact">
-                <h1 className="text-2xl mb-4">Contact Information</h1>
-                <div className="newContactInfo flex justify-between">
-                <Stack spacing={6}>
-                    <Input placeholder='First Name' size='lg' width="40rem"/>
-                    <Input placeholder='Account Name' size='lg' width="40rem"/>
-                    <Input placeholder='Email' size='lg' width="40rem"/>
-                    <Input placeholder='Work Phone' size='lg' width="40rem"/>
-                    <Input placeholder='Mobile Phone' size='lg' width="40rem"/>
-                    <Input placeholder='Assistant Name' size='lg' width="40rem"/>
-                </Stack>
-                <Stack spacing={6}>
-                    <Input placeholder='Last Name' size='lg' width="40rem"/>
-                    <Input placeholder='Title' size='lg' width="40rem"/>
-                    <Input placeholder='Department' size='lg' width="40rem"/>
-                    <Input placeholder='Fax' size='lg' width="40rem"/>
-                    <Input placeholder='Date of Birth' size='lg' width="40rem" type="date"/>
-                    <Input placeholder='Asst Phone' size='lg' width="40rem"/>
 
-                </Stack>
-                <button onClick={()=>check()}>check connection</button>
-                </div>
-            </div>
+  return (
+    <>
+      <CreateNewNav page="Contact" />
+
+      <div className="new-container">
+        <div className="new-PFP">
+          <h1 className="text-2xl">Contact Image</h1>
+          <div className="contactpfp">
+            <img src={pfp} alt="PFP" />
+          </div>
         </div>
-        </>
-    ) 
+        <div className="newContact">
+          <h1 className="text-2xl mb-4">Contact Information</h1>
+          <div className="newContactInfo flex justify-between">
+            <div className="input-container">
+              <Stack spacing={6}>
+                {inputFields.slice(0, 6).map((field) => (
+                  <Input
+                    key={field.key}
+                    placeholder={field.label}
+                    size='lg'
+                    width="40rem"
+                    required={field.required}
+                    type={field.type}
+                    onChange={(e) => handleInputChange(field.key, e.target.value)}
+                  />
+                ))}
+              </Stack>
+            </div>
+            <div className="input-container">
+              <Stack spacing={6}>
+                {inputFields.slice(6).map((field) => (
+                  <Input
+                    key={field.key}
+                    placeholder={field.label}
+                    size='lg'
+                    width="40rem"
+                    required={field.required}
+                    type={field.type}
+                    onChange={(e) => handleInputChange(field.key, e.target.value)}
+                  />
+                ))}
+              </Stack>
+            </div>
+            <button onClick={()=>{check()}}>Submit</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
